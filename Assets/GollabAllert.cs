3cb61dca921959ca6bbcbd1f7e1f428ee4be0b3e
A.H.Kashani <amkkashani@gmail.com>
2022-08-06 12:48:50 +0430

    alert system completed

---
+++ Assets/GollabAllert.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GollabAllert : MonoBehaviour
+{
+    [SerializeField] private Image alert;
+
+    [SerializeField]private int numberOfAlertedElements = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnOffTheAlarm();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void turnOnTheAlarm()
+    {
+        alert.color = Color.red;
+    }
+
+    private void turnOffTheAlarm()
+    {
+        alert.color = Color.black;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("building"))
+        {
+            turnOnTheAlarm();
+            numberOfAlertedElements++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("building"))
+        {
+            numberOfAlertedElements--;
+        }
+
+        if ( numberOfAlertedElements ==0)
+        {
+            turnOffTheAlarm();
+        }
+        
+    }
+}
